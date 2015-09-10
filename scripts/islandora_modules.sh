69e8c18b5d2650c22bd9e27de12dcb5d0385df46
unknown <zrrm74@MUL-3790.col.missouri.edu>
2015-09-10 10:38:17 -0500

    Change branch of node_export to clone Add additional modules to drush enable

--- scripts/islandora_modules.sh
+++ scripts/islandora_modules.sh
@@ -49,11 +49,10 @@ cd "$DRUPAL_HOME"/sites/all/modules/
 
 # Clone node export 
 cd "$DRUPAL_HOME"/sites/all/modules/
-git clone --branch master http://git.drupal.org/project/node_export.git
-cd node_export 
+git clone --branch 7.x-3.x http://git.drupal.org/project/node_export.git
+cd node_export
 git config core.filemode false
 cd "$DRUPAL_HOME"/sites/all/modules/
-
 
 # Clone libraries
 cd "$DRUPAL_HOME"/sites/all
@@ -121,10 +120,8 @@ drush -y -u 1 en islandora_book_batch islandora_pathauto islandora_pdfjs islando
 drush -y -u 1 en xml_forms islandora_scholar
 drush -y -u 1 en islandora_fits islandora_ocr islandora_oai islandora_marcxml islandora_xacml_editor islandora_xmlsitemap islandora_internet_archive_bookreader islandora_bagit islandora_batch islandora_newspaper_batch 
 drush -y -u 1 en google_analytics_reports islandora_bookmark islandora_bulk_operations islandora_ga_reports islandora_image_annotation islandora_importer islandora_ip_embargo islandora_jodconverter islandora_jwplayer islandora_mapping islandora_openseadragon islandora_paged_content islandora_plupload islandora_simple_workflow
-drush -y -u 1 en umkcdora umkc_feature_types umkc_islandora_browse
+drush -y -u 1 en umkcdora umkc_feature_types topics_and_types umkc_content_types umkc_browse
 
-#module islandora_openseadragon doesn't meet req. to be enabled. library Openseadragon missing
-#umkc_feature_types depends on umkc_content_types --(included w/ umkc_browse from philred's repo)
 
 #umkc browse depends on node_export_features
 
