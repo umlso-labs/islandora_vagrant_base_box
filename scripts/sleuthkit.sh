5e504f10e9a25aed2638a150b653cd0ad637f768
unknown <zrrm74@MUL-3790.col.missouri.edu>
2015-09-15 11:47:04 -0500

    More detailed comments about sleuthkit build fix

--- scripts/sleuthkit.sh
+++ scripts/sleuthkit.sh
@@ -16,7 +16,8 @@ apt-get install libafflib-dev afflib-tools libewf-dev ewf-tools -y --force-yes
 
 # Clone and compile Sleuthkit
 # TODO: https://github.com/sleuthkit/sleuthkit/pull/352/files
-# w/ sed
 cd /tmp
-git clone https://github.com/sleuthkit/sleuthkit.git
+git clone -b master https://github.com/sleuthkit/sleuthkit.git
+#modify /tmp/sleuthkit/bindings/java/ivysettings.xml
+#modify /tmp/sleuthkit/bindings/java/ivy.xml
 cd sleuthkit && ./bootstrap && ./configure && make && make install && ldconfig
