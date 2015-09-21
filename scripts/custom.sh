ae137e6118fad68415cbf50e454287bb4bb081db
unknown <zrrm74@MUL-3790.col.missouri.edu>
2015-09-21 11:46:33 -0500

    Install remaining drupal contrib modules in custom.sh

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -25,3 +25,15 @@ done < "$SHARED_DIR"/configs/islandora-theme-list-umlso.txt
 cd "$DRUPAL_HOME"/sites/all
 mkdir -p files/ctools/css 
 chmod 777 files/ctools/css
+
+echo "Installing Drupal contrib modules" 
+cd "$DRUPAL_HOME"/sites/all/modules
+drush dl i18n admin_menu advanced_help block_class entity entityreference exclude_node_title extlink feeds git_deploy image_link_formatter linkchecker securelogin views_slideshow views_slideshow_galleria openid_selector
+# Install openid-selector in libraries directory
+cd "$DRUPAL_HOME"/sites/all/libraries
+wget https://openid-selector.googlecode.com/files/openid-selector-1.3.zip
+unzip openid-selector-1.3.zip
+rm openid-selector-1.3.zip
+cd "$DRUPAL_HOME"/sites/all/modules
+# Enable the modules
+drush en i18n admin_menu advanced_help block_class entity entityreference exclude_node_title extlink feeds git_deploy image_link_formatter linkchecker securelogin views_slideshow views_slideshow_galleria openid_selector
