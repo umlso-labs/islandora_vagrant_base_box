3b4460f7be187134f82b8ffae2c191c4a5085cd0
Zachary Rump <nihilanth41@gmail.com>
2015-09-28 11:52:00 -0500

    Loop through site-names and do a site-install with drush

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -58,20 +58,40 @@ drush -y en i18n admin_menu job_scheduler advanced_help block_class entity entit
 # Suppress apache2 error about ServerName 
 sudo echo "ServerName localhost" >> /etc/apache2/apache2.conf
 
+# Setup multi-site 
+DOMAIN_NAME=localhost 
+cd $DRUPAL_HOME
+sites_arr=( lso merlin mospace mst mu umkc umsl ) 
+for i in "${sites_arr[@]}"
+do
+	drush si -y --db-url=mysql://root:islandora@localhost/"$i" --sites-subdir="$DOMAIN_NAME.$i" --site-name="$i"
+	# Set password for each site 
+	cd "$DRUPAL_HOME/sites/$DOMAIN_NAME.$i"
+	drush user-password admin --password=islandora
+	# Create symlinks
+	cd $DRUPAL_HOME
+	if [ -f $i ]; then
+		rm $i
+	fi
+	ln -s . $i 
+done
+
+
+# Enable islandora modules for the new site 
+# drush @sites -u 1 -y en <modules> 
+
+# Configure filter-drupal.xml for the new site by adding connection line 
+# ex <connection server="localhost" port="3306" dbname="testsite" user="root" password="islandora">
+# sudo service tomcat6 restart 
+
 # Set permissions (See: https://github.com/Islandora-Labs/islandora_vagrant/issues/76)
 sudo chown -hR vagrant:www-data "$DRUPAL_HOME"/sites/all/modules
 sudo chmod -R 755 "$DRUPAL_HOME"/sites/all/libraries
 sudo chmod -R 755 "$DRUPAL_HOME"/sites/all/modules
 
-# Setup multi-site 
-#site_arr=( lso merlin mospace mst mu umkc umsl ) 
-#for i in "${site_arr[@]}"
-#do
-#	drush si -y --db-url=mysql://root:islandora@localhost/"$i" --sites-subdir="$i" --site-name="example.org/$i"
-#done
-# Each site-install sets the admin password. Set it once at the end: 
-#drush user-password admin --password=islandora
 
+# Following steps necessary for subdomain sites or if doing install manually
+# Ex) test.localhost as opposed to localhost/test 
 #https://www.drupal.org/node/823990
 # Setup /etc/hosts 
 #sudo echo "127.0.0.1 testsite.localhost" >> /etc/hosts 
@@ -84,7 +104,6 @@ sudo chmod -R 755 "$DRUPAL_HOME"/sites/all/modules
 
 # Setup database for testsite (MYSQL)
 # Do this by hand (for now)
-
 
 # Setup sites folders 
 # Assert default/files exists 
@@ -100,15 +119,4 @@ sudo chmod -R 755 "$DRUPAL_HOME"/sites/all/modules
 
 # Replace the following with something automated ??
 # Go to testsite.localhost:8000/install.php 
-
-# Enable islandora modules for the new site 
-# drush @sites -u 1 -y en <modules> 
-
-# Configure filter-drupal.xml for the new site by adding connection line 
-# ex <connection server="localhost" port="3306" dbname="testsite" user="root" password="islandora">
-# sudo service tomcat6 restart 
-
-
-
-
 
